7725e0626b89781a86709a14d74efa68bc1bb211
wangxiaobo <457117157@qq.com>
2018-03-30 12:04:04 +0800

    commit 服务环境

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
  */
 import Koa from "koa";
 const staticCache = require('koa-static-cache');
+
 import Router from 'koa-router';
 import koaBody from 'koa-body';
 import fs from 'fs';
@@ -12,16 +13,28 @@ import jwt from 'koa-jwt';
 const app = new Koa();
 const router = new Router();
 
-import {indexController} from './controller/pages/router';
+import {indexController,searchController,rssController,collectController,hotController,reviseController,mineController,registerController} from './controller/pages/router';
+app.use(koaBody({
+    multipart: true
+}));
 app.keys = ['ylb'];
-
+//页面路由
 router.get('/index',indexController);
+router.get('/search',searchController);
+router.get('/rss',rssController);
+router.get('/collect',collectController);
+router.get('/hot',hotController);
+router.get('/revise',reviseController);
+router.get('/mine',mineController);
+router.get('/regist',registerController);
 app.use(cors());
 app
     .use(router.routes())
     .use(router.allowedMethods());
-app.use(staticCache('./build', {
-    maxAge: 365 * 24 * 60 * 60,
+
+
+app.use(staticCache(path.join('./states'), {
+    maxAge: 365 * 24 * 60 *60,
     dynamic: true,
     gzip: true
 }));
--- controller/pages/router.js
+++ controller/pages/router.js
@@ -3,10 +3,45 @@
  */
 //import {getIndexTemplate} from '../../template/index.html';
 import fs from 'fs';
-let getIndexTemplate = fs.createReadStream('./template/index.html');
+let getIndexTemplate = fs.readFileSync('./template/index.html');
 function indexController(ctx, next) {
     ctx.type = 'text/html';
     ctx.body = getIndexTemplate;
 }
-
-export {indexController}
+let getSearchTemplate = fs.readFileSync('./states/search.html');
+function searchController(ctx, next) {
+    //console.log(getSearchTemplate);
+    ctx.type = 'text/html';
+    ctx.body = getSearchTemplate;
+}
+let getRssTemplate = fs.readFileSync('./states/rss.html');
+function rssController(ctx, next) {
+    ctx.type = 'text/html';
+    ctx.body = getRssTemplate;
+}
+let getCollectTemplate = fs.readFileSync('./states/collect.html');
+function collectController(ctx,next){
+    ctx.type = 'text/html';
+    ctx.body = getCollectTemplate;
+}
+let getHotTemplate = fs.readFileSync('./states/hot.html');
+function hotController(ctx,next){
+    ctx.type = 'text/html';
+    ctx.body = getHotTemplate;
+}
+let getReviseTemplate = fs.readFileSync('./states/revise.html');
+function reviseController(ctx,next){
+    ctx.type = 'text/html';
+    ctx.body = getReviseTemplate;
+}
+let getMineTemplate = fs.readFileSync('./states/my.html');
+function mineController(ctx,next){
+    ctx.type = 'text/html';
+    ctx.body = getMineTemplate;
+}
+let getRegisterTemplate = fs.readFileSync('./states/regist.html');
+function registerController(ctx,next){
+    ctx.type = 'text/html';
+    ctx.body = getRegisterTemplate;
+}
+export {indexController,searchController,rssController,collectController,hotController,reviseController,mineController,registerController}
